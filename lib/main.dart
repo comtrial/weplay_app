b16fb1bfdafc6fe71088a431288add29b6657aae
Son-Seung-Tae <sonsttt@naver.com>
2021-05-19 16:04:03 +0900

    son first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weplay_app/binding/init_binding.dart';
+import 'package:weplay_app/screen/home_screen.dart';
 import 'package:weplay_app/widget/bottom_bar.dart';
+import 'package:weplay_app/widget/youtube_detail.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,23 +18,321 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      title: 'Weplay',
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          body: 
-          TabBarView(
-            //스크롤로 탭 간 전환 방지
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              Container()
-            ],
-          ),
-          bottomNavigationBar: Bottom(),
+    return GetMaterialApp(
+        title: 'Weplay',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.white,
+          accentColor: Colors.white,
         ),
-      )
-    );
+        initialBinding: InitBinding(),
+        initialRoute: "/",
+        getPages: [
+          GetPage(name: "/detail/:videoId", page: () => YoutubeDetail())
+        ],
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              //스크롤로 탭 간 전환 방지
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                HomeScreen(),
+                Container(),
+                Container(),
+                Container()
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          ),
+        ));
   }
 }
+
+// import 'dart:developer';
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/rendering.dart';
+// import 'package:flutter/services.dart';
+// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setSystemUIOverlayStyle(
+//     const SystemUiOverlayStyle(
+//       statusBarColor: Colors.blueAccent,
+//     ),
+//   );
+//   runApp(YoutubePlayerDemoApp());
+// }
+
+// /// Creates [YoutubePlayerDemoApp] widget.
+// class YoutubePlayerDemoApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Youtube Player Flutter',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         appBarTheme: const AppBarTheme(
+//           color: Colors.blueAccent,
+//           textTheme: TextTheme(
+//             headline6: TextStyle(
+//               color: Colors.white,
+//               fontWeight: FontWeight.w300,
+//               fontSize: 20.0,
+//             ),
+//           ),
+//         ),
+//         iconTheme: const IconThemeData(
+//           color: Colors.blueAccent,
+//         ),
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// /// Homepage
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   YoutubePlayerController _controller;
+//   TextEditingController _idController;
+//   TextEditingController _seekToController;
+
+//   PlayerState _playerState;
+//   YoutubeMetaData _videoMetaData;
+//   double _volume = 100;
+//   bool _muted = false;
+//   bool _isPlayerReady = false;
+
+//   final List<String> _ids = [
+//     'nPt8bK2gbaU',
+//     'gQDByCdjUXw',
+//     'iLnmTe5Q2Qw',
+//     '_WoCV4c6XOE',
+//     'KmzdUe0RSJo',
+//     '6jZDSSZZxjQ',
+//     'p2lYr3vM_1w',
+//     '7QUtEmBT_-w',
+//     '34_PXCzGw1M',
+//   ];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = YoutubePlayerController(
+//       initialVideoId: _ids.first,
+//       flags: const YoutubePlayerFlags(
+//         mute: false,
+//         autoPlay: true,
+//         disableDragSeek: false,
+//         loop: false,
+//         isLive: false,
+//         forceHD: false,
+//         enableCaption: true,
+//       ),
+//     )..addListener(listener);
+//     _idController = TextEditingController();
+//     _seekToController = TextEditingController();
+//     _videoMetaData = const YoutubeMetaData();
+//     _playerState = PlayerState.unknown;
+//   }
+
+//   void listener() {
+//     if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+//       setState(() {
+//         _playerState = _controller.value.playerState;
+//         _videoMetaData = _controller.metadata;
+//       });
+//     }
+//   }
+
+//   @override
+//   void deactivate() {
+//     // Pauses video while navigating to next page.
+//     _controller.pause();
+//     super.deactivate();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     _idController.dispose();
+//     _seekToController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return YoutubePlayer(
+//       controller: _controller,
+//       showVideoProgressIndicator: true,
+//       progressIndicatorColor: Colors.blueAccent,
+//       topActions: <Widget>[
+//         const SizedBox(width: 8.0),
+//         Expanded(
+//           child: Text(
+//             _controller.metadata.title,
+//             style: const TextStyle(
+//               color: Colors.white,
+//               fontSize: 18.0,
+//             ),
+//             overflow: TextOverflow.ellipsis,
+//             maxLines: 1,
+//           ),
+//         ),
+//         IconButton(
+//           icon: const Icon(
+//             Icons.settings,
+//             color: Colors.white,
+//             size: 25.0,
+//           ),
+//           onPressed: () {
+//             log('Settings Tapped!');
+//           },
+//         ),
+//       ],
+//       onReady: () {
+//         _isPlayerReady = true;
+//       },
+//       onEnded: (data) {
+//         _controller.load(_ids[(_ids.indexOf(data.videoId) + 1) % _ids.length]);
+//         _showSnackBar('Next Video Started!');
+//       },
+//     );
+//     builder:
+//     (context, player) => Scaffold(
+//             appBar: AppBar(
+//           leading: Padding(
+//             padding: const EdgeInsets.only(left: 12.0),
+//             child: Image.asset(
+//               'assets/ypf.png',
+//               fit: BoxFit.fitWidth,
+//             ),
+//           ),
+//           title: const Text(
+//             'Youtube Player Flutter',
+//             style: TextStyle(color: Colors.white),
+//           ),
+//           actions: [
+//             // IconButton(
+//             //   icon: const Icon(Icons.video_library),
+//             //   onPressed: () => Navigator.push(
+//             //     context,
+//             //     CupertinoPageRoute(
+//             //       builder: (context) => VideoList(),
+//             //     ),
+//             //   ),
+//             // ),
+//           ],
+//         ));
+//   }
+
+//   Widget _text(String title, String value) {
+//     return RichText(
+//       text: TextSpan(
+//         text: '$title : ',
+//         style: const TextStyle(
+//           color: Colors.blueAccent,
+//           fontWeight: FontWeight.bold,
+//         ),
+//         children: [
+//           TextSpan(
+//             text: value,
+//             style: const TextStyle(
+//               color: Colors.blueAccent,
+//               fontWeight: FontWeight.w300,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Color _getStateColor(PlayerState state) {
+//     switch (state) {
+//       case PlayerState.unknown:
+//         return Colors.grey[700];
+//       case PlayerState.unStarted:
+//         return Colors.pink;
+//       case PlayerState.ended:
+//         return Colors.red;
+//       case PlayerState.playing:
+//         return Colors.blueAccent;
+//       case PlayerState.paused:
+//         return Colors.orange;
+//       case PlayerState.buffering:
+//         return Colors.yellow;
+//       case PlayerState.cued:
+//         return Colors.blue[900];
+//       default:
+//         return Colors.blue;
+//     }
+//   }
+
+//   Widget get _space => const SizedBox(height: 10);
+
+//   Widget _loadCueButton(String action) {
+//     return Expanded(
+//       child: MaterialButton(
+//         color: Colors.blueAccent,
+//         onPressed: _isPlayerReady
+//             ? () {
+//                 if (_idController.text.isNotEmpty) {
+//                   var id = YoutubePlayer.convertUrlToId(
+//                         _idController.text,
+//                       ) ??
+//                       '';
+//                   if (action == 'LOAD') _controller.load(id);
+//                   if (action == 'CUE') _controller.cue(id);
+//                   FocusScope.of(context).requestFocus(FocusNode());
+//                 } else {
+//                   _showSnackBar('Source can\'t be empty!');
+//                 }
+//               }
+//             : null,
+//         disabledColor: Colors.grey,
+//         disabledTextColor: Colors.black,
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(vertical: 14.0),
+//           child: Text(
+//             action,
+//             style: const TextStyle(
+//               fontSize: 18.0,
+//               color: Colors.white,
+//               fontWeight: FontWeight.w300,
+//             ),
+//             textAlign: TextAlign.center,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _showSnackBar(String message) {
+//     ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(
+//         content: Text(
+//           message,
+//           textAlign: TextAlign.center,
+//           style: const TextStyle(
+//             fontWeight: FontWeight.w300,
+//             fontSize: 16.0,
+//           ),
+//         ),
+//         backgroundColor: Colors.blueAccent,
+//         behavior: SnackBarBehavior.floating,
+//         elevation: 1.0,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(50.0),
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/widget/video_widget.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class VideoWidget extends StatelessWidget {
+  const VideoWidget({Key key}) : super(key: key);
+
+  Widget _thumbnail() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      height: 250,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: Image.network(
+                    'https://i.pinimg.com/originals/86/ec/51/86ec51c589d572db18481beddfc8188a.jpg')
+                .image,
+            fit: BoxFit.cover),
+      ),
+    );
+  }
+
+  Widget _simpleDetailinfo() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      padding: const EdgeInsets.only(left: 10, bottom: 15),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            backgroundImage: Image.network(
+                    'https://pbs.twimg.com/profile_images/600556005462650881/BQr5YI4D_400x400.jpg')
+                .image,
+          ),
+          SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text("짱구짱구짱구짱구짱구", maxLines: 2),
+                    ),
+                    IconButton(
+                        alignment: Alignment.topCenter,
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.more_vert,
+                          size: 18,
+                        ))
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "하위하윙",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    Text(" . "),
+                    Text(
+                      "조회수",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    Text(" . "),
+                    Text(
+                      "2021-02-05",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          _thumbnail(),
+          _simpleDetailinfo(),
+        ],
+      ),
+    );
+  }
+}
