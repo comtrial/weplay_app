f8d0b870fb29e5cde05afde20b522fbb9e558a51
gilhan <rlfgks97@gmail.com>
2021-05-20 17:02:34 +0900

    First_commit/YGH

--- lib/model/model_video.dart
+++ lib/model/model_video.dart
@@ -1,14 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Video {
   final String title;
   final String channel_title;
   final String views;
   final String date;
+  final DocumentReference reference;
 
-  Video.fromMap(Map<String, dynamic> map)
+  Video.fromMap(Map<String, dynamic> map, {this.reference})
       : title = map['title'],
         channel_title = map['channel_title'],
         views = map['views'],
         date = map['date'];
+
+  // firebase와 연동하는 코드
+  Video.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
 
   @override
   String toString() => "Video<$title>";
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,21 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:weplay_app/widget/custom_appbar.dart';
 import 'package:weplay_app/widget/video_widget.dart';
 import 'package:get/get.dart';
+import 'package:weplay_app/model/model_video.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Stream<QuerySnapshot> streamData;
   @override
   void initState() {
     super.initState();
+    streamData = firestore.collection('video_data').snapshots();
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _fetchData(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('video_data').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildBody(context, snapshot.data.docs);
+      },
+    ); // StreamBuilder
+  }
+
+  // 데이터베이스에서 video를 videos에 리스트형태로 변환.
+  Widget _buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    List<Video> videos = snapshot.map((d) => Video.fromSnapshot(d)).toList();
+    print(videos);
     return SafeArea(
       child: CustomScrollView(
         slivers: [
@@ -38,5 +55,10 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _fetchData(context);
   }
 }
